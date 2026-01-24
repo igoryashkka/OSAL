a81cf93a08a3308ef4401e8eff5dfaa690a6f4b9
igoryashkka <igor404ik@gmail.com>
2026-01-24 16:34:41 +0200

    build/flash fix for black pill stm32f103, delete trash , update launch file

--- platform/stm32/impl/stm32f1/STM32F103x_Gpio_API.c
+++ platform/stm32/impl/stm32f1/STM32F103x_Gpio_API.c
@@ -4,15 +4,16 @@
 void GPIO_InitLed(void)
 {
     //LED_GPIO_CLK_ENABLE();
+    __HAL_RCC_GPIOB_CLK_ENABLE();
     GPIO_InitTypeDef GPIO_InitStruct = {0};
-    GPIO_InitStruct.Pin = 13;
+    GPIO_InitStruct.Pin = GPIO_PIN_12;
     GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
     GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
-    HAL_GPIO_Init(GPIOC, &GPIO_InitStruct);
-    HAL_GPIO_WritePin(GPIOC, 13, GPIO_PIN_RESET);
+    HAL_GPIO_Init(GPIOB, &GPIO_InitStruct);
+    HAL_GPIO_WritePin(GPIOB, GPIO_PIN_12, GPIO_PIN_RESET);
 }
 
 void GPIO_ToggleLed(void)
 {
-    HAL_GPIO_TogglePin(GPIOC, 13);
+    HAL_GPIO_TogglePin(GPIOB, GPIO_PIN_12);
 }
